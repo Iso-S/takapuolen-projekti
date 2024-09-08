2b72f723c4049b2a62dd4c4d3c0b91e35a9d4b1f
Ossi <saurio.ossi@gmail.com>
2024-09-08 16:50:51 +0300

    Added plain text and info to the frontpage template

---
+++ src/main/java/os/sa/takapuolenprojekti/TakapuolenprojektiApplication.java
@@ -0,0 +1,13 @@
+package os.sa.takapuolenprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakapuolenprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakapuolenprojektiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/os/sa/takapuolenprojekti/TakapuolenprojektiApplicationTests.java
@@ -0,0 +1,13 @@
+package os.sa.takapuolenprojekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakapuolenprojektiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
